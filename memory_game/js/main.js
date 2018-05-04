5dfef3bbb11b86edb57af863facdd48987670215
SimonAlbani <simon.albani2018@gmail.com>
2018-05-04 11:18:36 -0400

    finalized JS script in game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,13 +32,22 @@ var checkForMatch = function () {
         return;
     }
 }
-var flipCard = function (cardId) {
+var flipCard = function () {
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
     console.log("User Flipped " + cards[cardId].rank)
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
     checkForMatch();
 }
-
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        document.getElementById('game-board').appendChild(cardElement);
+        cardElement.addEventListener('click', flipCard);
+    }
+}
+createBoard();
